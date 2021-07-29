393a014cc0b6b07645cc820be6fd7cdfa6f07038
Willyan Ferdinando <willyantferdinando@gmail.com>
2021-07-29 20:59:44 -0300

    Controller Category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,58 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %w[show edit update destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.json { head :no_content }
+        format.js
+      else
+        format.json { render json: @category.erros.full_messages, status: :unprocessable_entity }
+        format.js { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.json { head :no_content }
+        format.js
+      else
+        format.json { render json: @category.erros.full_messages, status: :unprocessable_entity }
+        format.js { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:nome, :descricao)
+  end
+end
